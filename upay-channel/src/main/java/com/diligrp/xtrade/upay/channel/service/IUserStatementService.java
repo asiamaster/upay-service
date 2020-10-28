87cc6af81fa9b1ee7815d6e267614e9bceb55af9
huanggang <huanggang@diligrp.com>
2020-10-28 16:15:19 +0800

    Add user statement filter

--- upay-channel/src/main/java/com/diligrp/xtrade/upay/channel/service/IUserStatementService.java
+++ upay-channel/src/main/java/com/diligrp/xtrade/upay/channel/service/IUserStatementService.java
@@ -2,12 +2,14 @@ package com.diligrp.xtrade.upay.channel.service;
 
 import com.diligrp.xtrade.upay.channel.domain.SumUserStatement;
 import com.diligrp.xtrade.upay.channel.domain.UserStatementDto;
+import com.diligrp.xtrade.upay.channel.domain.UserStatementFilter;
 import com.diligrp.xtrade.upay.channel.domain.UserStatementQuery;
+import com.diligrp.xtrade.upay.channel.model.UserStatement;
 
 import java.util.List;
 
 /**
- * 渠道流水服务接口
+ * 客户账单服务接口
  *
  * @author: brenthuang
  * @date: 2020/10/14
@@ -22,4 +24,14 @@ public interface IUserStatementService {
      * 查询客户总收入和总支出
      */
     SumUserStatement sumUserStatements(UserStatementQuery query);
+
+    /**
+     * 根据交易号和账号ID查询客户交易（非"退款"）账单
+     */
+    UserStatementDto findUserStatement(UserStatementFilter filter);
+
+    /**
+     * 查询客户退款账单
+     */
+    List<UserStatementDto> listRefundStatements(UserStatementFilter filter);
 }
