5cdd09854b80bddc8461565360eb50732986f6c8
huanggang <huanggang@diligrp.com>
2021-06-15 14:44:31 +0800

    deposit permission support

--- upay-sentinel/src/main/java/com/diligrp/xtrade/upay/sentinel/domain/DepositSentinel.java
+++ upay-sentinel/src/main/java/com/diligrp/xtrade/upay/sentinel/domain/DepositSentinel.java
@@ -1,6 +1,7 @@
 package com.diligrp.xtrade.upay.sentinel.domain;
 
 import com.diligrp.xtrade.upay.core.ErrorCode;
+import com.diligrp.xtrade.upay.core.type.Permission;
 import com.diligrp.xtrade.upay.sentinel.exception.RiskControlException;
 
 /**
@@ -29,6 +30,9 @@ public class DepositSentinel extends Sentinel {
 
     @Override
     void checkPassport(Passport passport) {
+        if (!Permission.hasPermission(passport.getPermission(), Permission.FOR_DEPOSIT)) {
+            throw new RiskControlException(ErrorCode.OPERATION_NOT_ALLOWED, "风控提示：用户账号无提现权限");
+        }
         // 充值金额不能超过单笔充值限额
         if (maxAmount != null && passport.getAmount() > maxAmount) {
             throw new RiskControlException(ErrorCode.OPERATION_NOT_ALLOWED, "风控提示：充值金额超过单笔充值限额");
